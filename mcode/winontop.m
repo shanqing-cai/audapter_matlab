7d6b3714d1e2bb6588c92d119f0c507828bb82b2
shanqing-cai <shanqing.cai@gmail.com>
2014-01-13 12:44:18 -0500

    Some clean-up

--- mcode/winontop.m
+++
@@ -1,8 +0,0 @@
-%WINONTOP
-%   WINONTOP(FH) set "topmost" property of the figure specified by handle
-%   FH to state "on".
-%
-%   WINONTOP(FH,1) same as WINONTOP(FH)
-%
-%   WINONTOP(FH,0) set "topmost" property of the figure specified by handle
-%   FH to state "off".
